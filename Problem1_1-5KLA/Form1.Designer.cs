34162595ba55d560013dcb4cdae147d840021e8b
Kenneth <kennethlagarin@iskolarngbayan.pup.edu.ph>
2022-04-29 16:27:17 +0800

    first commit

---
+++ Problem1_1-5KLA/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace MyName
+{
+    partial class MyName
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(194, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Your Name";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MyName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 190);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Name = "MyName";
+            this.Text = "MyName";
+            this.Load += new System.EventHandler(this.MyName_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ Problem1_1-5KLA/Form1.cs
@@ -0,0 +1,20 @@
+namespace MyName
+{
+    public partial class MyName : Form
+    {
+        public MyName()
+        {
+            InitializeComponent();
+        }
+
+        private void MyName_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kenneth Laceda Agarin","My Name");
+        }
+    }
+}
